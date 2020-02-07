2df482f1d806ca95c1c63bc08d232210ae495450
SusejR <rodriguezbsusej@gmail.com>
2020-02-06 22:17:58 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    TrabajosComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
