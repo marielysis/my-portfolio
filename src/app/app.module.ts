79a79e1e9b530a1e7d5898593fda0316c0185501
SusejR <rodriguezbsusej@gmail.com>
2020-02-24 13:40:06 -0300

    Modificando segun recomendaciones de coach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
